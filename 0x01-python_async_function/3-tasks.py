637e66bf3fa669d1c2b0d311d846813fe5b28cbb
samiyonas <samuel.x.yonas@gmail.com>
2024-08-26 16:22:33 +0300

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" tasks """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ returns coroutine object """
+    task = asyncio.create_task(wait_random(max_delay))
+
+    return task
