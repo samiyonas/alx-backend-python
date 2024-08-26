46cff86eb1679c6aed4720af831d1b9684e117e3
samiyonas <samuel.x.yonas@gmail.com>
2024-08-26 14:05:22 +0300

    basic syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ The basics of async """
 import random, aysncio
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> Union[int, float]:
     """ delays for a random value between 0 and max_delay """
     rnd_time = await random.uniform(0, max_delay)
     return rnd_time
