f0a198d7b59e52a262f038071016a16719a7e651
samiyonas <samuel.x.yonas@gmail.com>
2024-09-23 15:35:42 -0700

    Parameterize a unit test (exception version)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, result):
         """ This method tests access_nested_map function """
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand(
+            [
+                ({}, ("a",), KeyError),
+                ({"a": 1}, ("a", "b"), KeyError)
+                ]
+            )
+    def test_access_nested_map_exception(self, nested_map, path, result):
+        """ tests that KeyError is raised for the above inputs """
+        with self.assertRaises(result):
+            access_nested_map(nested_map, path)
