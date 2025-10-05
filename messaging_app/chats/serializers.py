658d6fcc4c5381853f0f1f189dec39ac0901658e
samiyonas <samuel.x.yonas@gmail.com>
2025-10-05 12:58:26 +0300

    added a serializer class for several models

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,17 +4,19 @@ from .models import user, Message, Conversation
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = user
-        fields = '__all__'
+        fields = ['first_name', 'last_name', 'email', 'phone_number']
         read_only_fields = ['user_id', 'created_at']
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
     class Meta:
         model = Message
-        fields = '__all__'
-        read_only_fields = ['message_id', 'sender_id', 'sent_at']
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        read_only_fields = ['message_id', 'sent_at', 'sender']
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
     class Meta:
         model = Conversation
-        fields = '__all__'
-        read_only_fields = ['conversation_id', 'participants_id', 'created_at']
+        fields = ['conversation_id', 'participants', 'created_at']
+        read_only_fields = ['conversation_id', 'participants', 'created_at']
